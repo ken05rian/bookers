78673f46bdfb66a5b5b8132a1fc5d7c7cb8fc7fc
GitHubのユーザ名 <GitHub>
2021-01-11 06:12:57 +0000

    1stcommit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'top/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmltop
+  resources :books
+  
+  root to: 'top#index'
+ 
+end
+
